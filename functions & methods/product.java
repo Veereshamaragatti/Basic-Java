3095e356fc099737ddafd4910fe4af1c35e984b7
Veereshamaragatti <87874396+Veereshamaragatti@users.noreply.github.com>
2024-06-15 23:53:56 +0530

    Java
    Java

---
+++ functions & methods/product.java
@@ -0,0 +1,12 @@
+public class product {
+    public static int calculateMultiply(int a,int b){
+                int product=a*b;
+                return product;
+        }
+        public static void main(String args[]){
+            int a=20;
+            int b=30;
+            int product = calculateMultiply(5, 10);
+            System.out.println(product);
+        }
+    }
