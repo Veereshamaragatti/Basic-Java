d790eb55bc2d84cca1fe578264ee126d5a41f9d9
Veereshamaragatti <87874396+Veereshamaragatti@users.noreply.github.com>
2024-08-16 23:47:54 +0530

    java
    java

---
+++ array/MejorityElement.java
@@ -0,0 +1,20 @@
+public class MejorityElement {
+    public static int majorityElement(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j])
+                count++;
+            }
+            if(count>n/2){
+                 return nums[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int nums[]={2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+}
