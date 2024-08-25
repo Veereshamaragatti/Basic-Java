3361c00169839282185370848ecb7ee36a97f0ed
Veereshamaragatti <87874396+Veereshamaragatti@users.noreply.github.com>
2024-08-25 08:44:02 +0530

    java
    java

---
+++ 2Darray/matrixDiagonalSum.java
@@ -0,0 +1,17 @@
+public class matrixDiagonalSum {
+    public static int diagonalSum(int[][] mat) {
+        int n=mat.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];//for secondary diagonal
+            sum+=mat[i][n-1-i];//for primary diagonal
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        int mat[][]={{1,2,3},
+              {4,5,6},
+              {7,8,9}};
+        diagonalSum(mat);
+    }
+}
